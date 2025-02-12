959fea9020c8f9950c235219c43add03a7df8825
Alexandra Camero <acamerobejar@umass.edu>
2025-02-12 18:36:03 -0500

    Updated student links

--- CSC209/js/studentsInfo.js
+++ CSC209/js/studentsInfo.js
@@ -7,7 +7,7 @@ const STUDENT_SITES= {
 "Hillary"   : "https://hillarydunkley16.github.io/CSC209_Class/CSC209/startPage.html", 
 "Jade"      : "https://gemstone496.github.io/CSC209/start_page.html",
 "Karen"     : "#", 
-"Katie"     : "#", 
+"Katie"     : "https://kat1ev.github.io/209/Weeks/Week1/homePage.html", 
 "Matheus"   : "https://mcdasilva.github.io/CSC209/startPage.html", 
 "Michelle"  : "https://lawsonm525.github.io/CSC209/startPage.html",
 "Mikayle"   : "#", 
