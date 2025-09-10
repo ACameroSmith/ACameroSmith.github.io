0bb86345ba94c91e3bdc97467233a6bfa742eb14
acamerobejar <acamerobejar@umass.edu>
2025-09-10 18:54:07 -0400

    Added new students (Fall 2025)

--- CSC209/js/createLinks.js
+++ CSC209/js/createLinks.js
@@ -1,9 +1,20 @@
 function createStudentLinks(parentDiv, students) {
   let str = "";
-  let i = 1;
+  let i = 20;
   for (student in students) {
-    str += LINK_TEMPLATE.replace("LINK", students[student]).replace("NAME", student).replace("ICON", i);
-    i++;
+    str += LINK_TEMPLATE.replace("LINK", students[student]).replace("NAME", student).replaceAll("ICON", i);
+    i--;
   }
   parentDiv.innerHTML = str;
+}
+function setListeners(parentDiv) {
+  let icons = parentDiv.querySelectorAll(".student-icon");
+  icons.forEach(icon=> {
+    icon.parentNode.addEventListener("mouseover", () => {
+      icon.src = icon.src.replace("Dark", "Light");
+    });
+    icon.parentNode.addEventListener("mouseout", () => {
+      icon.src = icon.src.replace("Light", "Dark");
+    });
+  });
 }
