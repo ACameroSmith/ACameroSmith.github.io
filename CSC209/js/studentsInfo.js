bf899f6a042cb0a78a1932e3bbcaa949ac38ec7a
Alexandra Camero <acamerobejar@umass.edu>
2026-09-21 18:57:23 -0400

    New student names Fall 2026

--- CSC209/js/studentsInfo.js
+++ CSC209/js/studentsInfo.js
@@ -1,11 +1,13 @@
 const STUDENT_SITES= {
-"Ayesha"    : "https://ayesha-saleem-sheikh.github.io/CSC209/startPage.html", 
-"Cybele"    : "https://cbourelly.github.io/CSC209/startPage.html", 
-"Fernanda"  : "https://jaltamiranobello.github.io/CSC209/startPage.html", 
-"Glory"     : "https://yougangglory.github.io/CSC209/startPage.html", 
-"Isabelle"  : "https://isabellelwang.github.io/CSC209/startPage.html", 
-"Jessica"   : "https://jklurfeld.github.io/CSC209/", 
-"Melissa"   : "https://mkabalisa.github.io/CSC209/startPage.html",
-"Vivian"  	: "https://vstreber.github.io/VStreberSmith.github.io/CSC209/startPage.html", 
-"Wen"     	: "https://wentingchua.github.io/csc209.github.io/CSC209/startPage.html"
+"Ani"    	: "", 
+"Aryn"    	: "", 
+"Catherine" : "", 
+"Jennifer" 	: "", 
+"Jode" 		: "", 
+"Kylie" 	: "", 
+"Milla" 	: "", 
+"Piper" 	: "", 
+"Reed" 		: "", 
+"Tabitha" 	: "",
+"Cybele"    : "https://cbourelly.github.io/CSC209/startPage.html"
 }
