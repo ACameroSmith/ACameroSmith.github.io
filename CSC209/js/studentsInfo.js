071f3db00fc6609b1bcb2b0a0dca38f77f75ab0e
Alexandra Camero <acamerobejar@umass.edu>
2026-09-24 18:18:55 -0400

    Updated Aryn's link

--- CSC209/js/studentsInfo.js
+++ CSC209/js/studentsInfo.js
@@ -1,6 +1,6 @@
 const STUDENT_SITES= {
 "Ani"    	: "https://abrunourena.github.io/CSC209/main.html", 
-"Aryn"    	: "https://arynvarris.github.io/CSC209/Hw1/home.html", 
+"Aryn"    	: "https://arynvarris.github.io/CSC209/home.html", 
 "Catherine" : "https://catherinetweeks.github.io/CSC209/main.html", 
 "Jennifer" 	: "https://jen927.github.io/CSC209/main.html",  
 "Jode" 		: "", 
